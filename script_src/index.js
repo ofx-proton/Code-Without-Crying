9930eb4ccb9318526a979c2ad028fc67608e2c0a
Mr Proton <251812551+ofx-proton@users.noreply.github.com>
2026-01-03 14:41:04 +0545

    Homepage Added

---
+++ script_src/index.js
@@ -0,0 +1,49 @@
+// Initialize the required DOM elements
+const navbarMenu = document.getElementById("menu");
+const burgerMenu = document.getElementById("burger");
+const headerMenu = document.getElementById("header");
+const bgOverlay = document.querySelector(".overlay");
+
+// Toggle navbar menu
+const toggleNavbarMenu = () => {
+   navbarMenu.classList.toggle("is-active");
+   burgerMenu.classList.toggle("is-active");
+   bgOverlay.classList.toggle("is-active");
+};
+
+// Burger click
+burgerMenu.addEventListener("click", () => {
+   toggleNavbarMenu();
+});
+
+// Overlay click
+bgOverlay.addEventListener("click", () => {
+   toggleNavbarMenu();
+});
+
+// Close menu when any menu area is clicked
+document.querySelectorAll(".menu").forEach((link) => {
+   link.addEventListener("click", () => {
+      toggleNavbarMenu();
+   });
+});
+
+// Header background on scroll
+window.addEventListener("scroll", () => {
+   if (window.scrollY >= 75) {
+      headerMenu.classList.add("on-scroll");
+   } else {
+      headerMenu.classList.remove("on-scroll");
+   }
+});
+
+// Fix menu state on resize
+window.addEventListener("resize", () => {
+   if (window.innerWidth >= 768) {
+      if (navbarMenu.classList.contains("is-active")) {
+         navbarMenu.classList.remove("is-active");
+         burgerMenu.classList.remove("is-active");
+         bgOverlay.classList.remove("is-active");
+      }
+   }
+});
